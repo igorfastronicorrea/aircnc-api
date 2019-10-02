4e04d36ca694b52c44fc18356396d12f69c24bd4
Igor Fastroni <igor.fastroni@gmail.com>
2019-10-02 20:26:37 -0300

    add rotas de criacao e listagem spot e users

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const uploadConfig = require('./config/upload');
+const multer = require('multer');
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashBoardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+routes.post('/sessions', SessionController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+
+routes.get('/dashboard', DashBoardController.show);
+
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express();
 
-app.use(express.json());
-
-app.post('/users/', (req, res) => {
-    return res.json(req.body);
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-3yhsq.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
